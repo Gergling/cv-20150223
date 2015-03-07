3b36f47ba69149f847419a8854530b89e274e582
gergling <gregmichaeldavies@gmail.com>
2015-03-07 20:45:09 +0000

    Included fusepump as job.

--- src/public/module/section-jobs/service/list.js
+++ src/public/module/section-jobs/service/list.js
@@ -13,7 +13,8 @@ angular.module('section-jobs').service("section-jobs.service.list", [
             azuragroup: {"label": "AzuraGroup", "title": "Web Developer", start: "2007", period: "2007 - 2008"},
             aviva: {"label": "Aviva", "title": "Front-end Developer", start: "2008", period: "2008 - June 2009"},
             unanimis: {"label": "Unanimis", "title": "PHP Developer", start: "2009", period: "June 2009 - June 2012"},
-            gointeractive: {"label": "GoInteractive", "title": "Web Applications Developer", start: "2012", period: "June 2012 - Present"},
+            gointeractive: {"label": "GoInteractive", "title": "Web Applications Developer", start: "2012", period: "June 2012 - May 2014"},
+            fusepump: {"label": "FusePump", "title": "Senior Frontend Developer", start: "2014", period: "June 2014 - Present"}
         };
         this.order = {date:[]};
         angular.forEach(this.list, function(company, companyName) {
