9c2a7222be819037f6985a23951d387344e6e2a4
gergling <gregmichaeldavies@gmail.com>
2015-03-09 12:10:11 +0000

    Removed superfluous code.

--- src/public/module/section-skills/service/list.js
+++ src/public/module/section-skills/service/list.js
@@ -57,38 +57,6 @@ angular.module('section-skills').service("section-skills.service.list", [
                 sf.ims("lotus-notes", "Lotus Notes"),
                 sf.ims("jira", "JIRA")
             ],
-            /*skills = {
-                jasmine: {label: "Jasmine", typeName: "tool"},
-                grunt: {label: "Grunt", typeName: "tool"},
-                express: {label: "Express", typeName: "framework", parent: "mean"},
-                mean: {label: "MEAN Stack", typeName: "stack"},
-                nodejs: {label: "Node JS", typeName: "language", parent: "mean"},
-                javascript: {label: "Javascript", typeName: "language"},
-                angularjs: {label: "AngularJS", typeName: "framework", language: "javascript", parent: "mean"},
-                requirejs: {label: "RequireJS", typeName: "framework", language: "javascript"},
-                backbone: {label: "Backbone", typeName: "framework", language: "javascript"},
-                jQuery: {label: "jQuery", typeName: "framework", language: "javascript"},
-                threejs: {label: "Three.js", url: "http://threejs.org/", typeName: "framework", language: "javascript"},
-                chartjs: {label: "DevExpress ChartJS", url: "http://chartjs.devexpress.com/", typeName: "framework", language: "javascript"},
-                html: {label: "HTML/HTML5", typeName: "language"},
-                css: {label: "CSS3", typeName: "language"},
-                ubuntu: {label: "Linux (Ubuntu)", typeName: "os", parent: "lamp"},
-                mysql: {label: "MySQL", typeName: "language", parent: "lamp"},
-                phpmyadmin: {label: "PHPMyAdmin", typeName: "tool", language: "mysql"},
-                workbench: {label: "Workbench", typeName: "tool", language: "mysql"},
-                mongo: {label: "Mongo", typeName: "language", parent: "mean"},
-                phpmoadmin: {label: "PHPMoAdmin", typeName: "tool", language: "mongo"},
-                shell: {label: "Bash/Shell", typeName: "language"},
-                batch: {label: "Batch Scripting", typeName: "language"},
-                scite: {label: "SciTE", typeName: "text-editor"},
-                svn: {label: "SVN", typeName: "subversion"},
-                tfs: {label: "TFS", typeName: "subversion"},
-                "git-subversion": {label: "GIT Subversion", typeName: "subversion", url: "https://github.com/Gergling"},
-                "git-issues": {label: "GIT Issues", typeName: "issue-management", url: "https://github.com/Gergling"},
-                lotusNotes: {label: "Lotus Notes", typeName: "issue-management"},
-                jira: {label: "JIRA", typeName: "issue-management"},
-                bootstrap: {label: "Bootstrap", typeName: "framework", language: "css"}
-            },*/
             types = {
                 "ims": {label: "Issue Management"},
                 srcControl: {label: "Source Control"},
@@ -100,8 +68,6 @@ angular.module('section-skills').service("section-skills.service.list", [
                 framework: {label: "Framework"}
             };
 
-        //scope.order = { alphabetical: [ ] };
-
         skills.forEach(function (skill) {
             skill.typeIcon = "icon-cog";
             skill.type = types[skill.typeName];
@@ -109,11 +75,6 @@ angular.module('section-skills').service("section-skills.service.list", [
             if (glyphiconMap[skill.typeName]) {
                 skill.typeIcon = "glyphicon glyphicon-" + glyphiconMap[skill.typeName];
             }
-            /*skill.toggle = function () {
-                skill.selected = !skill.selected;
-            };*/
-
-            //scope.order.alphabetical.push(skill);
         });
 
         this.types = function (name) {
--- src/public/module/section-skills/service/skill.js
+++ src/public/module/section-skills/service/skill.js
@@ -42,20 +42,5 @@ angular.module('section-skills').service("section-skills.service.skill", [
                 return create(name, label, fncName, obj, { language: language });
             };
         });
-        /*this.stack = function (name, label, obj) {
-            return create(name, label, "stack", obj);
-        };
-        this.language = function (name, label, obj) {
-            return create(name, label, "language", obj);
-        };*/
-        this.framework = function (name, label, language, obj) {
-            return create(name, label, "framework", obj, { language: language });
-        };
-        this.library = function (name, label, language, obj) {
-            return create(name, label, "library", obj, { language: language });
-        };
-        //this.tool = function (name, label, obj) {
-            //return create(name, label, "tool", obj);
-        //};
     }
 ]);
