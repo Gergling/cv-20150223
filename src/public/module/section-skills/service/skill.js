bd4dc17dbf5df5e373971988e45bf6c452558107
gergling <gregmichaeldavies@gmail.com>
2015-03-09 14:06:55 +0000

    Skill categories restructured.

--- src/public/module/section-skills/service/skill.js
+++ src/public/module/section-skills/service/skill.js
@@ -1,10 +1,34 @@
 angular.module('section-skills').service("section-skills.service.skill", [
 
-    function () {
+    "$filter",
+
+    function ($filter) {
 
         "use strict";
 
         var scope = this,
+
+            category = function (name, label, glyphicon, hasLanguage) {
+                var category = { };
+                category.name = name;
+                category.label = label;
+                category.glyphicon = glyphicon || "cog";
+                category.hasLanguage = hasLanguage;
+                return category;
+            },
+            categories = [
+                category("framework", "Framework", "gift", true),
+                category("ide", "Development Environment"),
+                category("ims", "Issue Management", "road"),
+                category("language", "Language", "lamp"),
+                category("library", "Library", "gift", true),
+                category("os", "Operating System"),
+                category("srcControl", "Source Control", "road"),
+                category("stack", "Stack"),
+                category("tool", "Tool", "wrench"),
+                category("webserver", "Webserver"),
+            ],
+
             Skill = function () {
                 this.toggle = function () {
                     this.selected = !this.selected;
@@ -16,31 +40,30 @@ angular.module('section-skills').service("section-skills.service.skill", [
                 obj = obj || { };
                 skill.name = name;
                 skill.label = label;
+                skill.category = (function () {
+                    var cat = $filter('filter')(categories, { name: typeName })[0];
+                    if (!cat) {
+                        throw new Error("section-skills.service.skill: No category '" + typeName + "'");
+                    }
+                    return cat;
+                }());
                 skill.typeName = typeName;
                 return angular.extend(skill, presets, obj);
             };
 
-        [
-            "ide",
-            "ims",
-            "language",
-            "os",
-            "srcControl",
-            "stack",
-            "tool"
-        ].forEach(function (fncName) {
-            scope[fncName] = function (name, label, obj) {
-                return create(name, label, fncName, obj);
-            };
-        });
+        categories.forEach(function (cat) {
+            var fncName = cat.name,
+                fnc = function (name, label, obj) {
+                    return create(name, label, fncName, obj);
+                };
 
-        [
-            "framework",
-            "library"
-        ].forEach(function (fncName) {
-            scope[fncName] = function (name, label, language, obj) {
-                return create(name, label, fncName, obj, { language: language });
-            };
+            if (cat.hasLanguage) {
+                fnc = function (name, label, language, obj) {
+                    return create(name, label, fncName, obj, { language: language });
+                };
+            }
+
+            scope[fncName] = fnc;
         });
     }
 ]);
